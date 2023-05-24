a3c6fba25565e4ae9e287948625711b25917cb3e
colornos <rivas432@hotmail.com>
2023-05-23 21:57:12 -0500

    Update start.py

--- start.py
+++ start.py
@@ -1,17 +1,30 @@
 import subprocess
 import sys
+import importlib
 
-def install(package):
+def install_python_package(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-# List of necessary packages
-packages = ["torch", "numpy", "transformers", "datasets", "tiktoken", "wandb", "tqdm"]
+def install_system_package(package):
+    subprocess.check_call(["sudo", "apt-get", "install", "-y", package])
 
-for package in packages:
+# List of necessary Python packages
+python_packages = ["torch", "numpy", "transformers", "datasets", "tiktoken", "wandb", "tqdm"]
+
+for package in python_packages:
     try:
-        dist = __import__(package)
+        dist = importlib.import_module(package)
         print("{} ({}) is installed".format(dist.__name__, dist.__version__))
     except ImportError:
         print("{} is NOT INSTALLED".format(package))
         print("Installing {}...".format(package))
-        install(package)
+        install_python_package(package)
+
+# Check for vim
+try:
+    subprocess.check_call(["vim", "--version"])
+    print("vim is installed")
+except subprocess.CalledProcessError:
+    print("vim is NOT INSTALLED")
+    print("Installing vim...")
+    install_system_package("vim")
