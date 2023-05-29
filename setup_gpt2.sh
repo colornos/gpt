c74e4689cc31d40fdb438987e97a7a213573c297
colornos <rivas432@hotmail.com>
2023-05-29 15:13:55 -0500

    Update setup_gpt2.sh

--- setup_gpt2.sh
+++ setup_gpt2.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-set -e
 
 # Update the system and install necessary system packages
 sudo apt update
 sudo apt upgrade -y
-sudo apt install -y vim python3-dev libpq-dev curl build-essential libffi-dev libssl-dev zlib1g-dev libreadline-dev libsqlite3-dev wget llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev python3-pip python3-venv
+sudo apt install -y vim python3-dev libpq-dev curl build-essential libffi-dev python3-pip
 
-# Install pyenv for Python version management if it does not already exist
-if [ ! -d "$HOME/.pyenv" ] ; then
-    curl https://pyenv.run | bash
-fi
+# Remove existing pyenv directory if exists
+rm -rf ~/.pyenv
+
+# Install pyenv for Python version management
+curl https://pyenv.run | bash
 
 # Add necessary lines to .bashrc for pyenv
-if ! grep -q 'export PYENV_ROOT="$HOME/.pyenv"' ~/.bashrc ; then
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.bashrc
-fi
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.bashrc
 
 # Reload .bashrc to apply changes
 source ~/.bashrc
@@ -27,19 +25,23 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 
 # Install Python 3.10.0 with pyenv and set it as the default
-if ! pyenv versions | grep -q '3.10.0' ; then
-    pyenv install 3.10.0
-    pyenv global 3.10.0
-fi
+pyenv install 3.10.0
+pyenv global 3.10.0
 
-# Create a Python virtual environment
-python3 -m venv gpt2_env
+# Create a new virtual environment
+python3 -m venv /home/roni/nanoGPT/gpt2_env
 
 # Activate the virtual environment
-source gpt2_env/bin/activate
+source /home/roni/nanoGPT/gpt2_env/bin/activate
 
-# Update pip, setuptools, and wheel in the virtual environment
-pip install --upgrade pip setuptools wheel
+# Update pip, setuptools, and wheel
+python3 -m pip install --upgrade pip setuptools wheel
 
-# Install necessary Python packages into the virtual environment
-pip install torch numpy transformers datasets tiktoken wandb tqdm
+# Install necessary Python packages
+python3 -m pip install torch numpy transformers datasets tiktoken wandb tqdm
+
+# Add alias to .bashrc to quickly activate the environment
+echo "alias activate_gpt2='source /home/roni/nanoGPT/gpt2_env/bin/activate'" >> ~/.bashrc
+source ~/.bashrc
+
+echo "Remember to activate the virtual environment with 'activate_gpt2'"
