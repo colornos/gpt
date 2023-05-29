050dedb81f92501ba900c9953b3afe26855fdad9
colornos <rivas432@hotmail.com>
2023-05-29 15:44:27 -0500

    Update setup_gpt2.sh

--- setup_gpt2.sh
+++ setup_gpt2.sh
@@ -1,47 +1,39 @@
 #!/bin/bash
+set -e
+
+# Make sure only root can run this script
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
 
 # Update the system and install necessary system packages
-sudo apt update
-sudo apt upgrade -y
-sudo apt install -y vim python3-dev libpq-dev curl build-essential libffi-dev python3-pip
+apt update
+apt upgrade -y
+apt install -y vim python3-dev libpq-dev curl build-essential libffi-dev libssl-dev zlib1g-dev libreadline-dev libsqlite3-dev wget llvm libncurses5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
 # Remove existing pyenv directory if exists
-rm -rf ~/.pyenv
+rm -rf /root/.pyenv
 
 # Install pyenv for Python version management
 curl https://pyenv.run | bash
 
 # Add necessary lines to .bashrc for pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.bashrc
+echo 'export PYENV_ROOT="/root/.pyenv"' >> /root/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> /root/.bashrc
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> /root/.bashrc
 
 # Reload .bashrc to apply changes
-source ~/.bashrc
+source /root/.bashrc
 
 # Sometimes necessary for pyenv to be recognized immediately
-export PYENV_ROOT="$HOME/.pyenv"
+export PYENV_ROOT="/root/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 eval "$(pyenv init --path)"
 
 # Install Python 3.10.0 with pyenv and set it as the default
-pyenv install 3.10.0
-pyenv global 3.10.0
-
-# Create a new virtual environment
-python3 -m venv /home/roni/nanoGPT/gpt2_env
-
-# Activate the virtual environment
-source /home/roni/nanoGPT/gpt2_env/bin/activate
+$PYENV_ROOT/bin/pyenv install 3.10.0
+$PYENV_ROOT/bin/pyenv global 3.10.0
 
 # Update pip, setuptools, and wheel
-python3 -m pip install --upgrade pip setuptools wheel
-
-# Install necessary Python packages
-python3 -m pip install torch numpy transformers datasets tiktoken wandb tqdm
-
-# Add alias to .bashrc to quickly activate the environment
-echo "alias activate_gpt2='source /home/roni/nanoGPT/gpt2_env/bin/activate'" >> ~/.bashrc
-source ~/.bashrc
-
-echo "Remember to activate the virtual environment with 'activate_gpt2'"
+$PYENV_ROOT/shims/python3 -m pip install --upgrade pip setuptools wheel
